636e7e84a9409476266904734fff600b85b76aeb
Ernesto Alves <98620218+Ernesto-Alves67@users.noreply.github.com>
2024-06-14 11:37:24 -0300

    [FRONT-PROTO] Esboço Index

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.multinavs"
+    namespace = "com.example.index2"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.multinavs"
+        applicationId = "com.example.index2"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
